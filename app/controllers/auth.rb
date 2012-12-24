a34624003b1ae374a61520e3f837010129b42ce2
hideack <hideaki@feeddown.com>
2012-12-24 15:27:31 +0900

    OAuth後にuserテーブルへ記録する様に修正

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,4 +1,5 @@
 Impas.controllers :auth do
+  # http://impas-hideack.sqale.jp/auth
   get :index do 
     url = oauthClient.auth_code.authorize_url(:redirect_uri => AUTH_REDIRECT_URL, :scope => 'user,public_repo')
     puts "Redirecting to URL: #{url.inspect}"
@@ -7,16 +8,33 @@ Impas.controllers :auth do
 
   # http://impas-hideack.sqale.jp/auth/callback
   get :callback do
-    redirectUrl = "http://local.impas-hideack.sqale.jp/auth/callback"
-    puts params[:code]
-
     begin
       access_token = oauthClient.auth_code.get_token(params[:code], :redirect_uri => AUTH_REDIRECT_URL)
       user = JSON.parse(access_token.get('/user').body)
-      "<p>Your OAuth access token: #{access_token.token}</p><p>Your extended profile data:\n#{user.inspect}</p>"
+
+      # ユーザ管理
+      reguser = User.where(:name => user["name"])
+
+      if reguser.count == 0
+        reguser = User.new
+        reguser.name = user["name"]
+        reguser.usertype = 0
+        reguser.save
+      else
+        reguser = reguser[0]
+      end
+
+      session[:user] = reguser
+
     rescue OAuth2::Error => e
       %(<p>Outdated ?code=#{params[:code]}:</p><p>#{$!}</p><p><a href="/auth/github">Retry</a></p>)
     end
+
+    redirect "/user"
+  end
+
+  get :logout do
+    session.delete(:user)
   end
 
   # get :index, :map => "/foo/bar" do
