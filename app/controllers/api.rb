1493500fde5eb611ecb5022d4fdad287560d77f0
hideack <hideaki@feeddown.com>
2012-12-29 23:04:38 +0900

    APIの枠とグループ追加のAPI実装

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -1,22 +1,41 @@
 Impas.controllers :api do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
 
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  # http://impas-hideack.sqale.jp/api/group/[operation key]
+  post :group, :with => :opkey do
+    checkOpKey(params[:opkey]) do
+      passedJson = request.body.read.force_encoding("utf-8")
+      return 400 if !validateJsonCommand('post_group', passedJson)
 
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
+      command = parseCommand(passedJson)
+      user = User.find_by_opkey(params[:opkey])
 
-  # get "/example" do
-  #   "Hello world!"
-  # end
+      # Group added.
+      hashseed = "#{command["name"]}-impasgrp-#{Time.now.to_i}"
+      
+      grp = Group.new
+      grp.userid = user.id
+      grp.key = Digest::MD5.new.update(hashseed).to_s
+      grp.name = command["name"]
+      grp.save
 
-  
+      # API response
+      generateResponse(true, {})
+    end
+  end
+
+
+  # http://impas-hideack.sqale.jp/api/group/[operation key]
+  get :group, :with => :opkey do
+  end
+
+  # http://impas-hideack.sqale.jp/api/url/[group key]
+  post :url, :with => [:key] do
+  end
+
+  # http://impas-hideack.sqale.jp/api/url/[group key]/[url]
+  get :url, :with => [:key, :url] do
+  end
+
+  get :ranking, :with => [:key, :type, :limit] do
+  end
 end
