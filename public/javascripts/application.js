7c031b6889b97eb15ae4dbfa6628fd3b52d46813
hideack <hideaki@feeddown.com>
2013-01-12 18:10:19 +0900

    グループをWebのUIから登録できる様に修正

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,21 @@
 // Put your application scripts here
+$(document).ready(function() {
+    $('#addGroup').click(function(n){
+        opKey = $('#opKey').html();
+        newName = $(this).prev().val();
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/group/' + opKey,
+            contentType: 'application/json',
+            data: JSON.stringify({ "name": newName}),
+            success: function(data){
+                location.href = "/user";
+            },
+            error: function(){
+                alert("グループの登録に失敗しました");
+            }
+        });
+    });
+});
+
