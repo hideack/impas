ef33cb229cf5d9785233cb500ebbeebf32ac9d48
hideack <hide.terai@gmail.com>
2013-04-07 16:15:20 +0900

    キャッシュ適用

--- app/app.rb
+++ app/app.rb
@@ -23,6 +23,7 @@ class Impas < Padrino::Application
   #   set :cache, Padrino::Cache::Store::Memory.new(50)
   #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
   #
+  set :cache, Padrino::Cache::Store::Memory.new(10240)
 
   ##
   # Application configuration options
